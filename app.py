04dde12f5fa427304669fae5a68ac3e9a21de88c
vighneshyedam <77835417+vighneshyedam@users.noreply.github.com>
2023-01-17 14:35:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # Store this code in 'app.py' file
 from flask import Flask, render_template, request, redirect, url_for, session#jsonify
-from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 import sqlite3
